8b1879899a6ec324b7a78ca3011632153ced1a20
Michele In The Code <miinthecode@gmail.com>
2025-05-29 11:59:51 -0300

    banco de dados

--- index.js
+++ index.js
@@ -1,46 +1,60 @@
 const express = require ('express')
 const app = express()      //a biblioteca possui uma função. para simplicar transforma na função app
 const port = 6579          // nao pode usar porta que ja esta sendo usada, por padrao porta 3000
-const produtosDb = []
-const usersDb = []
+const { Pool } = require('pg')  // usada para importar os comandos de conexão
 
-app.use(express.json())
+const pool = new Pool({           // Quando o projeto rodar, ele vai criar uma nova conexão 
+    user: 'postgres.erzwfacsvpporqmdsasc',
+    host: 'aws-0-sa-east-1.pooler.supabase.com', 
+    database: 'postgres', 
+    password: 'Mi01042025',
+    port: 5432
+})        
 
-app.get("/", (req, res) => {    // sempre que essa rota for acessa, vai aparecer uma informação. // arrowfunction, função auto executável
-    res.send("Hi Michele!")    // req e res, são obrigatório o uso no backend
-})     
+app.use(express.json())           // criar as rotas para inserir produtos
+app.post('/produtos', async (req, res) => {                   //async, habilita a funcao await onde vai esperar retornar o requisição para depois seguir
+    const {nome, preco, categoria, image_url} = req.body
 
-app.post("/produtos", (req, res) => {       // O usuário tem que mandar 3 informações. 
-    const {nome, preco, categoria} = req.body            //a forma mais fácil de extrair os dados, dentro do req.body vai ter as infos
-
-    if(!nome || !preco || !categoria){
-        return res.status(400).send("Nome, preco ou categoria são obrigatórios!")
+    if(!nome || !preco || !categoria || !image_url){
+        return res.status(400).send('Todos os campos são obrigatórios!')
+    }
+    if (nome.lenght > 100){                  // lenght faz uma leitura de quantos caracteres tem no lista e diz a quantidade. 
+        return res.status(400).send('Nome tem que contar no máximo 100 caracteres.')     // vai ler quantos caracteres tem e apresentar mensagem de erro caso maior que 100
+    }
+    if (categoria.length > 50){
+        return res.status(400).send('Categoria tem que conter no máximo 50 caracteres')
     }
 
-    const produto = {
-        nome: nome,
-        preco: preco, 
-        categoria: categoria
+    try {
+        const produto = await pool.query(`
+        INSERT INTO produtos (nome, preco, categoria, image_url)
+        VALUES (
+        '${nome}',
+        ${preco},
+        '${categoria}', 
+        '${image_url}'
+        )
+        RETURNING *        
+    `)                                  //inserir e returnar para mim o objeto que acabou de inserir
+
+    res.status(201).send(produto.rows[0])
+        
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send('Erro ao cadastrar o produto')
+    }      
+})
+
+app.get('/produtos', async(req, res) => {                       // rota para visualizar as informações da lista //nao existe req.body no GET
+    try {
+        const produtos = await pool.query('SELECT * FROM produtos')
+
+        return res.status(200).send(produtos.rows)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send('Erro ao buscar o produto') 
     }
-
-    produtosDb.push(produto)
-
-    res.status(201).send(produto)
-})
-
-app.get("/produtos", (req, res) => {
-    res.send(produtosDb)
-})
-
-app.post("/usuarios", (req, res) => {
-    const {nome, email, senha} = req.body
-
-    if(!nome || !email || !senha){  //se for diferente de nome, ou diferente de email, ou diferente de senha, vai parar a execução
-        return res.status(400).send("Email, nome ou senha são obrigatórios!")
-    }
-    usersDb.push({nome, email, senha})
-    res.status(201).send("Usuário cadastrado com sucesso!")
-})
+} ) 
 
 app.listen(port, () => {
     console.log(`O servidor está rodando na porta ${port}`)     // crase pois é uma string dinamica
